300dda7507b99ba5fc5ab1d51fd9610869f83226
cdevogel25 <cdevogel25@gmail.com>
2022-12-13 20:50:54 -0600

    day4

--- 2022/day4.py
+++ 2022/day4.py
@@ -4,15 +4,56 @@ def interpret(puzzle_input):
     new_input = []
     for item in puzzle_input:
         a = item.split(',')
-        print(a)
         for k in range(len(a)):
             a[k] = a[k].split('-')
             for n in range(len(a[k])):
                 a[k][n] = int(a[k][n])
         new_input.append(a)
-    print(new_input)
+    return new_input
+
+def contains(puzzle_input):
+    # check if left contains right, if right contains left
+    contains_sum = 0
+    for pair in puzzle_input:
+        # left:
+        if pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]:
+            contains_sum += 1
+            continue
+        if pair[1][0] >= pair[0][0] and pair[1][1] <= pair[0][1]:
+            contains_sum += 1
+            continue
+    
+    return contains_sum
+
+def overlap(puzzle_input):
+    overlap_sum = 0
+    for pair in puzzle_input:
+        if (pair[0][0] >= pair[1][0] and pair[0][0] <= pair[1][1]) or (pair[0][1] <= pair[1][1] and pair[0][1] >= pair[1][0]):
+            overlap_sum += 1
+            continue
+        if (pair[1][0] >= pair[0][0] and pair[1][1] <= pair[0][1]) or (pair[1][1] <= pair[0][1] and pair[1][0] >= pair[0][0]):
+            overlap_sum += 1
+            continue
+    
+    return overlap_sum
+
+def overlap_range(puzzle_input):
+    overlap_sum = 0
+    for pair in puzzle_input:
+        if pair[0][0] in range(pair[1][0],pair[1][1] + 1) or pair[0][1] in range(pair[1][0],pair[1][1] + 1):
+            overlap_sum += 1
+            continue
+        if pair[1][0] in range(pair[0][0],pair[0][1] + 1) or pair[1][1] in range(pair[0][0],pair[0][1] + 1):
+            overlap_sum += 1
+            continue
+    
+    return overlap_sum
+
 
 if __name__ == '__main__':
     pi = read_input('./day4_input.txt')
 
-    interpret(pi)
+    input_int = interpret(pi)
+    print(contains(input_int))
+    print(overlap(input_int))
+    print(overlap_range(input_int))
